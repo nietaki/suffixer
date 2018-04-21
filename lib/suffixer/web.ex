895bc1a19c84c1c069f954435980479ef67f4c37
Jacek Królikowski <nietaki@gmail.com>
2018-04-21 13:35:22 +0100

    Showing something on the home page

--- lib/suffixer/web.ex
+++ lib/suffixer/web.ex
@@ -1,9 +1,13 @@
 defmodule Suffixer.Web do
   use Ace.HTTP.Service, [cleartext: true]
 
-  def handle_request(_request = %{path: ["s", suffix]}, config) do
+  def handle_request(request = %{path: []}, config) do
+    handle_request(%{request | path: ["s", "on"]}, config)
+  end
+
+  def handle_request(_request = %{path: ["s", suffix]}, _config) do
     response(:ok)
-    |> set_body("foo\nbar\nbaz")
+    |> set_body("#{suffix}\n\nbar\nbaz")
   end
 
   def handle_request(_, _) do
